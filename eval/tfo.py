58f24145430e64fa0b1b4a3e2202cd6b3eeede03
bave <inoue.tomoya@gmail.com>
2013-08-24 14:56:31 +0900

    update

---
+++ eval/tfo.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import sys
+import socket
+
+PORT = 10443
+
+TCP_FASTOPEN = 23
+MSG_FASTOPEN = 0x20000000
+
+def server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_TCP, TCP_FASTOPEN, 5)
+
+    s.bind(("", PORT))
+    s.listen(socket.SOMAXCONN)
+
+    while True:
+        conn, addr = s.accept()
+        while True:
+            ret = conn.recv(1024)
+            if len(ret) <= 0:
+                break
+            print ret;
+            conn.sendall(ret)
+        conn.close()
+    s.close()
+
+
+def client():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.sendto("1. Using TCP fast open!!!", MSG_FASTOPEN, (sys.argv[2], PORT))
+    ret = s.recv(1024)
+    print ret
+
+    s.send("2. continued data sending")
+    ret = s.recv(1024)
+    print ret
+
+    s.close()
+
+def usage():
+    print 'Usage:'
+    print ' python tfo.py [option]'
+    print ''
+    print '   -s'
+    print '     server mode'
+    print ''
+    print '   -c [servername]'
+    print '     clinet mode'
+    print ''
+
+def main():
+    if '-s' in sys.argv:
+        server()
+    elif '-c' in sys.argv:
+        client()
+    else:
+        usage()
+
+if __name__ == '__main__':
+    main()
