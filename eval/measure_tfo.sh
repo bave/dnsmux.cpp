8b290faeb4101e2371e4cba754f351b15ed78e26
bave <inoue.tomoya@gmail.com>
2013-08-25 17:05:13 +0900

    add eval/measurement/* and script

--- eval/measure_tfo.sh
+++ eval/measure_tfo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LIST=`node -e " var hoge='hoge'; for (var i=0; i<=10000; i++) { console.log(hoge+i+'.my'); } "`
+LIST=`node -e " var hoge='hoge'; for (var i=0; i<=10000; i++) { console.log(hoge+i+'.mydns'); } "`
 
 for i in ${LIST}
 do
